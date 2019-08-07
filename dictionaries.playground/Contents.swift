9663de4f73d0b83b847892a45e094d876307147f
Apple <apple@Apples-MacBook-Pro-5.local>
2019-08-07 13:25:15 -0700

    initial commit

---
+++ dictionaries.playground/Contents.swift
@@ -0,0 +1,81 @@
+//import UIKit
+//
+//var str = "Hello, playground"
+//
+//var FriendsOfKarlie = [ "Michelle Obama" , "Serena Williams" , "T Swift" , "Jimmy Fallon" ]
+//for friend in FriendsOfKarlie {
+//    print( "hello \(friend)!")
+//}
+//var perfectTen : [ String : String ] = [
+//    "almond flour": "3 and 1/2 cups",
+//    "gluten-free oats" : "2 cups",
+//    "mini chocolate chips" : "1 cup"
+//    ]
+//print(perfectTen)
+//
+//var perfectTen : [String : String] = [:]
+//
+//perfectTen["almond flour"] = "3 and 1/2 cups"
+//perfectTen["gluten-free oats"] = "2 cups"
+//perfectTen["mini chocolate chips"] = "1 cup"
+//print(perfectTen.keys)
+//print(Array(perfectTen.keys))
+//
+//var sponsors = [ "adidas" , "estee lauder" , "carolina herrera good girl" , "apple" , "wework"]
+//
+//for sponsor in sponsors {
+//  print("shout out to \(sponsor) for help makng kwkw happen!")
+//}
+//var capitals = ["France" : "Paris" , "Cuba" : "Havana" , "Japan" : "Tokyo"]
+//for (country, capital) in capitals {
+//    print("the capital of \(country) is \(capital)")
+//}
+//for _ in  1...4 {
+//    print("hello!")
+//}
+//class Scholar {
+//    //code will go here
+//}
+//class Scholar {
+//    var grade = 10
+//    var studying = "swift"
+//    var name = ""
+//init(scholarName : String) {
+//    name = scholarName
+//}
+//}
+//    var newScholar = Scholar(scholarName : "Viola")
+//print("my name is \(newScholar.name)")
+//class Scholar {
+//    var studying = "Swift"
+//    var name = ""
+//    var grade = 0
+//
+//    init(scholarName : String, scholarGrade : Int) {
+//        name = scholarName
+//        grade = scholarGrade
+//}
+//func writeCode () {
+//    print("\(name) is busy writing code!")
+//}
+//}
+//var newScholar = Scholar(scholarName : "Amy" , scholarGrade : 11)
+//newScholar.writeCode()
+
+
+//class Jenna {
+//var studying = "coding"
+//var lastName = ""
+//var grade = 12
+//
+//    init(JennaLast : String , JennaGrade : Int) {
+//lastName = JennaLast
+//grade = JennaGrade
+//}
+//    func writeCode () {
+//        print("Jenna \(lastName) is in \(grade) grade")
+//}
+//}
+//var JennaNew = Jenna(JennaLast : "Dabaja" , JennaGrade : 12)
+//JennaNew.writeCode()
+
