09b4bcea00221825f91907e27306c8b55e1aac42
Apple <apple@Apples-MacBook-Pro-5.local>
2019-08-08 14:56:56 -0400

    message

---
+++ actions and outlets practice/actions and outlets practiceTests/actions_and_outlets_practiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  actions_and_outlets_practiceTests.swift
+//  actions and outlets practiceTests
+//
+//  Created by Apple on 8/8/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import actions_and_outlets_practice
+
+class actions_and_outlets_practiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
