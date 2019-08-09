868734710478b0e6255a9892306e5dc40763f09d
Apple <apple@Apples-MacBook-Pro-10.local>
2019-08-09 11:12:36 -0400

    message

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let emojis = ["🏝" : "palm tree" , "🌃" : "downtown" , "🏔" : "nature" , "🎢" : "Amusement Parks" , "❄️" : "cold", "🌎" : "random"]
+    let emojis = ["🏝" : "Tropical" , "🌃" : "City Life" , "🏔" : "Nature" , "🎢" : "Amusement Parks" , "❄️" : "Cold Places", "🌎" : "Random"]
     let customMessages =
-        ["palm tree" : ["Bahamas", "Fiji", "Bora Bora", "Hawaii", "Mexico", "Bali", "Punta Cana"],
-         "downtown" :  ["Chicago", "New York", "Paris", "Detroit", "Toronto", "Las Vegas", "Seattle"],
-         "nature" : ["Colorado", "Washington", "Montana","Arizona", "Alaska", "Tennessee", "Wyoming"], "Amusement Parks" : ["Cedar Point", "Six Flags", "Disneyland", "Canada's Wonderland", "Hersheypark", "Universal Studios", "Magic Kingdom"],
-         "cold" : ["Iceland", "Alaska", "Prague", "Berlin", "Toronto", "Norway", "Amsterdam"],
-         "random" : ["Spain", "Los Angeles", "Dubai", "Austria", "Brazil", "Argentina", "Japan"]]
+        ["Tropical" : ["Bahamas", "Fiji", "Bora Bora", "Hawaii", "Mexico", "Bali", "Punta Cana"],
+         "City Life" :  ["Chicago", "New York", "Paris", "Detroit", "Toronto", "Las Vegas", "Seattle"],
+         "Nature" : ["Colorado", "Washington", "Montana","Arizona", "Alaska", "Tennessee", "Wyoming"], "Amusement Parks" : ["Cedar Point", "Six Flags", "Disneyland", "Canada's Wonderland", "Hersheypark", "Universal Studios", "Magic Kingdom"],
+         "Cold Places" : ["Iceland", "Alaska", "Prague", "Berlin", "Toronto", "Norway", "Amsterdam"],
+         "Random" : ["Spain", "Los Angeles", "Dubai", "Austria", "Brazil", "Argentina", "Japan"]]
     
     @IBAction func sendMessage(sender: UIButton) {
         let selectedEmotion = sender.titleLabel!.text
         let random = Int.random(in: 0 ..< 7)
         let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
-        let alertController = UIAlertController(title: selectedEmotion , message: emojiMessage ,  preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: emojis[selectedEmotion!] , message: emojiMessage ,  preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
